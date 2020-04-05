49859ddb2362b43a6ca1536fe00751ee3708b2dc
Johnny Ramos <johnny.gerardo24@hotmail.com>
2020-04-05 08:05:26 -0500

    first commit

--- app/src/main/java/com/example/sceneform_demo/MainActivity.java
+++ app/src/main/java/com/example/sceneform_demo/MainActivity.java
@@ -21,6 +21,9 @@ import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.BaseArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
+import java.net.URI;
+import java.net.URISyntaxException;
+import tech.gusavila92.websocketclient.WebSocketClient;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -35,10 +38,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     ViewRenderable animal_name;
 
+    private WebSocketClient webSocketClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("WebSocket", "===================== INIT =================");
+        try {
+            createWebSocketClient();
+            Log.i("WebSocket", "===================== ENTRO ===============");
+        } catch (URISyntaxException e) {
+            Log.i("WebSocket", "===================== ERROR ===============");
+            e.printStackTrace();
+        }
 
         arFragment = (ArFragment)getSupportFragmentManager().findFragmentById(R.id.sceneform_ux_fragment);
 
@@ -63,6 +77,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 createModel(anchorNode, selected);
             }
         });
+
+
     }
 
     private void setupModel() {
@@ -143,5 +159,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         selected = 2;
+    }
+
+    private void createWebSocketClient() throws URISyntaxException {
+        URI uri;
+        Log.i("WebSocket", "===================== ENTRO 2 ===============");
+        uri = new URI("ws://signalserver.herokuapp.com");
+        Log.i("WebSocket", "===================== ENTRO 3 ===============");
+        webSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen() {
+                Log.i("WebSocket", "Session is starting");
+                webSocketClient.send("Hello World");
+            }
+
+            @Override
+            public void onTextReceived(String message) {
+                Log.i("WebSocket", "Message received");
+                Log.i("WebSocket", message);
+            }
+
+            @Override
+            public void onBinaryReceived(byte[] data) {
+
+            }
+
+            @Override
+            public void onPingReceived(byte[] data) {
+
+            }
+
+            @Override
+            public void onPongReceived(byte[] data) {
+
+            }
+
+            @Override
+            public void onException(Exception e) {
+                Log.i("WebSocket",  e.getMessage());
+                System.out.println(e.getMessage());
+            }
+
+            @Override
+            public void onCloseReceived() {
+                Log.i("WebSocket", "Closed ");
+                System.out.println("onCloseReceived");
+            }
+        };
+
+        webSocketClient.setConnectTimeout(10000);
+        webSocketClient.setReadTimeout(60000);
+        webSocketClient.enableAutomaticReconnection(5000);
+        webSocketClient.connect();
     }
 }
